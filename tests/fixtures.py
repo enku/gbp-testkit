dced936706a7621d027bfcf5f014f4d8654e9430
Albert Hopkins <marduk@letterboxes.org>
2025-02-24 19:58:03 -0600

    tests: add tests for the fixtures

---
+++ tests/fixtures.py
@@ -0,0 +1,20 @@
+# pylint: disable=missing-docstring
+from gbp_testkit import fixtures as testkit
+
+api_keys = testkit.api_keys
+build = testkit.build
+builds = testkit.builds
+build_model = testkit.build_model
+client = testkit.client
+clock = testkit.clock
+console = testkit.console
+environ = testkit.environ
+gbp = testkit.gbp
+jenkins = testkit.jenkins
+publisher = testkit.publisher
+pulled_builds = testkit.pulled_builds
+record = testkit.record
+records_db = testkit.records_db
+settings = testkit.settings
+storage = testkit.storage
+tmpdir = testkit.tmpdir
